3ff674368fec1eb9dc2536d37060ae19d1196469
frances elliott <fsc.elliott@gmail.com>
2021-09-29 14:48:11 -0600

    sync

--- constants.py
+++ constants.py
@@ -1,51 +1,24 @@
 #!/usr/local/bin/python
 import json
 
+# test constants:
 doc_type = "auto_insurance_quote_api_test"
 config_name = "anyco"
 document_url = "https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/pdfs/auto_insurance_anyco.pdf"
-config = json.dumps({
-  "fields": [
-    {
-      "id": "policy_period",
-      "anchor": "policy period",
-      "method": {
-        "id": "label",
-        "position": "right"
-      }
-    },
-    {
-      "id": "comprehensive_premium",
-      "anchor": "comprehensive",
-      "type": "currency",
-      "method": {
-        "id": "row",
-        "tiebreaker": "second"
-      }
-    },
-    {
-      "id": "property_liability_premium",
-      "anchor": "property",
-      "type": "currency",
-      "method": {
-        "id": "row",
-        "tiebreaker": "second"
-      }
-    },
-    {
-      "id": "policy_number",
-      "type": "number",
-      "anchor": {
-        "match": [
-          {
-            "text": "policy number",
-            "type": "startsWith"
-          }
-        ]
-      },
-      "method": {
-        "id": "box"
-      }
-    }
-  ]
-})
+doc_local_path = "auto_insurance_anyco.pdf"
+# constatns to publish:
+'''
+doc_type = "DOC_TYPE_NAME"
+config_name = "CONFIG_NAME"
+doc_local_path = "PDF/PATH.pdf"
+document_url = "https://DOC_URL.pdf"
+'''
+
+'''
+to test with examples:
+- In the Sensible app, create a doc type, then specify its name in doc_type
+- create a config in the doc type by pasting in the JSON from:  https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/json/anyco.json
+- for ASYNC: document_url = "https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/pdfs/auto_insurance_quote.pdf"
+- for SYNC: download the example [auto_insurance_quote PDF](https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/pdfs/auto_insurance_quote.pdf),
+  and define doc_local_path using the path to the downloaded PDF 
+'''
--- extract_doc.py
+++ extract_doc.py
@@ -3,56 +3,33 @@ import requests
 import time
 import json
 import sys
-from setup_config import *
 from constants import *
 from secrets import *
+import base64
 
 '''
 extract structured data from the example PDF specified in constants.py
 '''
 
-def extract_from_doc_url():  
-  url = "https://api.sensible.so/dev/extract_from_url/{}".format(doc_type)
-  payload = json.dumps({"document_url": document_url})
+def extract_from_local_doc():  
+  print(doc_local_path)
+  with open(doc_local_path, 'rb') as pdf_file:
+    encoded_string = base64.b64encode(pdf_file.read())
+    print(encoded_string)
+    pdf_file.close()
+  url = "https://api.sensible.so/dev/extract/{}".format(doc_type)
+  payload= encoded_string
   headers = {
-    'Authorization': 'Bearer {}'.format(API_KEY),  'Content-Type': 'application/json'
-  }
-  print ("Initiating asyn request to extract from doc at url {}\n".format(document_url))
+  'Authorization': 'Bearer {}'.format(API_KEY),
+  'Content-Type': 'application/pdf'
+}
+
   try:
     response  = requests.request("POST", url, headers=headers, data=payload)
     response.raise_for_status()
   except requests.RequestException as err:
     print(response.text)
     raise SystemExit(err)
-  extraction_id = response.json()['id']
-  return extraction_id
-  
-def retrieve_extraction(id):
-  print('Retrieving extracted data from extraction id {}\n'.format(id))
-  url = "https://api.sensible.so/dev/documents/{}".format(id)
-  payload={}
-  headers = {
-    'Authorization': 'Bearer {}'.format(API_KEY)
-  }
-  try:
-    response  = requests.request("GET", url, headers=headers, data=payload)
-    response.raise_for_status()
-  except requests.RequestException as err:
-    print(response.text)
-    raise SystemExit(err)
-  # to avoid polling, implement a webhook in prod
-  time.sleep(5)
-  while "parsed_document" not in response.text:
-    print(response.json()["status"],"\n")
-    response = requests.request("GET", url, headers=headers, data=payload)
-    if response.json()["status"]=="FAILED":
-      print(json.dumps(response.json(), indent=2))
-      exit()
-    time.sleep(3)  
-  print('EXTRACTED DATA:\n')
   print(json.dumps(response.json(), indent=2))
-
-
 if __name__ == '__main__':
-  extraction_id = extract_from_doc_url()
-  retrieve_extraction(extraction_id)
+  extract_from_local_doc()
