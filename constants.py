1bd29d80f0e7fc58e812c66ffe387b05b2d83691
frances elliott <fsc.elliott@gmail.com>
2021-09-29 16:06:08 -0600

    polish

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import json
 
 # test constants for development:
 doc_type = "auto_insurance_quote"
-doc_local_path = "auto_insurance_anyco.pdf"
+doc_local_path = "TODELETE_auto_insurance_anyco.pdf"
 # constants for async extraction:
 document_url = "https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/pdfs/auto_insurance_anyco.pdf"
 
