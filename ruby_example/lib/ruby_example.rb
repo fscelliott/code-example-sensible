659dacffb6af3e6c8b43622af848deca305b5be2
frances elliott <fsc.elliott@gmail.com>
2021-10-06 12:10:48 -0600

    not object-oriented, but now uses method

--- ruby_example/lib/ruby_example.rb
+++ ruby_example/lib/ruby_example.rb
@@ -1,46 +1,58 @@
+#!/usr/bin/ruby 
 # frozen_string_literal: true
 
+# TODO: delete require_relative?
 require_relative "ruby_example/version"
 require "faraday"
-require "uri" # todo delete these
-require "net/http"
 require 'json'
-# require "dotenv"
 
-# Dotenv.load('./.env')
+#module RubyExample
+  #class Error < StandardError; end
 
-
-
-module RubyExample
-  class Error < StandardError; end
 
 # TODO: delete test vars on publish
 # specify your variable values here 
-doc_type = "auto_insurance_quote"
-doc_local_path = "../../TODELETE_auto_insurance_anyco.pdf"
+Doc_type = "auto_insurance_quote"
+Doc_local_path = "../../TODELETE_auto_insurance_anyco.pdf"
 # TODO: on publish, specify API key inline here instead + delete test vars
 # var API_KEY = "YOUR_API_KEY"
 API_KEY = ENV['API_KEY']
 
-begin 
-  pdf_bytes = IO.binread(doc_local_path)
-rescue IOError => e
-  fail(e)
-end 
+def self.extract_from_local_doc()
 
-url = URI("https://api.sensible.so/dev/extract/#{doc_type}")
 
-response = Faraday.post(url) do |req|
-  req.headers['Content-Type'] = 'application/pdf'
-  req.body = pdf_bytes
-  req.headers["Authorization"] = "Bearer #{API_KEY}"
-end 
-if !response.success?
-  abort "The request failed: #{response.status} #{response.reason_phrase}"
-end  
+  begin 
+    pdf_bytes = IO.binread(Doc_local_path)
+  rescue IOError => e
+    fail(e)
+  end 
 
-# TODO: is it a style problem that I don't tell Farady to expect response.body to be JSON?
-puts "EXTRACTED DATA"
-response_json = JSON.parse(response.body)
-puts JSON.pretty_generate(response_json)
+  url = URI("https://api.sensible.so/dev/extract/#{Doc_type}")
+
+  response = Faraday.post(url) do |req|
+    req.headers['Content-Type'] = 'application/pdf'
+    req.body = pdf_bytes
+    req.headers["Authorization"] = "Bearer #{API_KEY}"
+  end 
+  if !response.success?
+    abort "The request failed: #{response.status} #{response.reason_phrase}"
+  end  
+
+  # TODO: is it a style problem that I don't tell Farady to expect response.body to be JSON?
+  puts "EXTRACTED DATA"
+  response_json = JSON.parse(response.body)
+  puts JSON.pretty_generate(response_json)
+  
+
+end
+
+#end  
+
+
+if __FILE__ == $PROGRAM_NAME
+  size_Mb = File.size(Doc_local_path)/(1024*1024)
+  puts size_Mb
+  if size_Mb < 4.5
+    extract_from_local_doc()
+  end
 end
