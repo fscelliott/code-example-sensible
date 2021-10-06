07cf336d95c5f5980645c90dd2b1ec1efe0333f0
frances elliott <fsc.elliott@gmail.com>
2021-10-06 12:21:00 -0600

    polish

--- ruby_example/lib/ruby_example.rb
+++ ruby_example/lib/ruby_example.rb
@@ -2,14 +2,11 @@
 # frozen_string_literal: true
 
 # TODO: delete require_relative?
-require_relative "ruby_example/version"
+#require_relative "ruby_example/version"
 require "faraday"
 require 'json'
 
-#module RubyExample
-  #class Error < StandardError; end
-
-
+# TODO: style problem that I don't use modules & classes & just define some constants?
 # TODO: delete test vars on publish
 # specify your variable values here 
 Doc_type = "auto_insurance_quote"
@@ -19,16 +16,12 @@ Doc_local_path = "../../TODELETE_auto_insurance_anyco.pdf"
 API_KEY = ENV['API_KEY']
 
 def self.extract_from_local_doc()
-
-
   begin 
     pdf_bytes = IO.binread(Doc_local_path)
   rescue IOError => e
     fail(e)
   end 
-
   url = URI("https://api.sensible.so/dev/extract/#{Doc_type}")
-
   response = Faraday.post(url) do |req|
     req.headers['Content-Type'] = 'application/pdf'
     req.body = pdf_bytes
@@ -37,21 +30,19 @@ def self.extract_from_local_doc()
   if !response.success?
     abort "The request failed: #{response.status} #{response.reason_phrase}"
   end  
-
   # TODO: is it a style problem that I don't tell Farady to expect response.body to be JSON?
   puts "EXTRACTED DATA"
   response_json = JSON.parse(response.body)
-  puts JSON.pretty_generate(response_json)
-  
-
+  puts JSON.pretty_generate(response_json) 
 end
 
-#end  
-
-
 if __FILE__ == $PROGRAM_NAME
-  size_Mb = File.size(Doc_local_path)/(1024*1024)
-  puts size_Mb
+  # TODO: clean up error handling so I on check Doc_local_path 1x
+  begin 
+    size_Mb = File.size(Doc_local_path)/(1024*1024)
+  rescue IOError => e
+    fail(e)
+  end 
   if size_Mb < 4.5
     extract_from_local_doc()
   end
