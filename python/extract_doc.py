de68c6237deaed0443bf38bb3f7a3148a00a0f9d
frances elliott <fsc.elliott@gmail.com>
2021-10-06 09:48:45 -0600

    ruby auth working

--- python/extract_doc.py
+++ python/extract_doc.py
@@ -4,7 +4,6 @@ import json
 import os
 from secrets import API_KEY
 
-
 '''
 extract structured data from PDFs
 '''
--- ruby_example/lib/ruby_example.rb
+++ ruby_example/lib/ruby_example.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
 require_relative "ruby_example/version"
+# require "http"
+require "uri" # todo delete these
+require "net/http"
+# require "dotenv"
+
+# Dotenv.load('./.env')
 
 module RubyExample
   class Error < StandardError; end
-  # Your code goes here...
+
+
+API_KEY = ENV['API_KEY']
+puts "hi here's the key:"
+puts API_KEY
+
+url = URI("https://api.sensible.so/dev/extract/auto_insurance_quote")
+
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+
+request = Net::HTTP::Post.new(url)
+request["Authorization"] = "Bearer #{API_KEY}"
+request["Content-Type"] = "application/pdf"
+request.body = "<file contents here>"
+
+response = https.request(request)
+puts response.read_body
+
 end
